6d85a49aa5084be4a530dd676ca53ce3f84fd070
WilsonkwSheng <wilsonkhoo93@gmail.com>
2017-09-14 10:14:17 +0800

    create a view page for show that display the long and short url 1. create link_to navigation between index and show page

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -2,4 +2,9 @@ class UrlsController < ApplicationController
 	def index
 		@urls = Url.all
 	end
+
+	def show
+		# byebug <ActionController::Parameters {"controller"=>"urls", "action"=>"show", "id"=>"1"} permitted: false>
+		@url = Url.find(params[:id])
+	end
 end
