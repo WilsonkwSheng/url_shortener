dafbb59afc0cc130fb9a327ac48cbb3c05aacb2e
WilsonkwSheng <wilsonkhoo93@gmail.com>
2017-09-14 11:14:21 +0800

    create a form to convert long url to short url 1. added strong parameters method

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -1,10 +1,31 @@
 class UrlsController < ApplicationController
 	def index
 		@urls = Url.all
+		@url = Url.new
 	end
 
 	def show
 		# byebug <ActionController::Parameters {"controller"=>"urls", "action"=>"show", "id"=>"1"} permitted: false>
 		@url = Url.find(params[:id])
+	end
+
+	def new
+		@url = Url.new
+	end
+
+	def create
+		#byebug
+		@url = Url.new(url_params)
+		@url.shorten
+		if @url.save
+			redirect_to @url
+		else
+			render 'new'
+		end
+	end
+
+	private
+	def url_params
+		params.require(:url).permit(:long_url)
 	end
 end
--- app/models/url.rb
+++ app/models/url.rb
@@ -1,2 +1,27 @@
 class Url < ApplicationRecord
+	before_save :shorten #validation run shorten method
+	validates :long_url, presence: true, uniqueness: true
+	before_validation :check_long_url
+	validates :long_url, format: { with: /\A(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?\z/, message: "Invalid url"  }
+
+	def check_long_url
+		
+		if (!self.long_url.include? "http://") || (!self.long_url.include? "https://")
+			a = self.long_url.partition("www.") 
+			if a[1].include?("www.")
+				self.long_url = "https://" + self.long_url
+			else
+				self.long_url = self.long_url = "https://www." + self.longurl
+			end
+		else
+			#  add if else statement here 
+			self.long_url = "https://" + self.long_url
+		end
+	end
+
+	def shorten
+		if self.short_url.nil?
+			self.short_url = SecureRandom.hex(3) #self.shorturl = Url.shorturl
+		end
+	end
 end
